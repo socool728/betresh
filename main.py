006aad81a0442da12f9e29985d45bb2330f7a23f
Vvolfs <jaircarlos3302@gmail.com>
2023-08-01 11:02:37 -0600

    123

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import datetime
 from bs4 import BeautifulSoup
 import requests
 
-url = input("Enter URL")
+url = input("Enter URL: ")
 current_month = datetime.datetime.now().month
 current_year = datetime.datetime.now().year
 
 print(current_month - 6)
 
-response = requests.get("https://api.example.com/data")
+response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
 country = soup.find_all('a[href="javascript://"]')[1]
 print(country.text)
